98ae26aaa9ea1137be5778dab46f2f024dfe6ca9
Kamal Ibrahim <kokoangry51@gmail.com>
2025-09-11 18:32:29 +0300

    add Dio client (base options + LogInterceptor)

---
+++ lib/api/dio_client.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:crud_project/api/endpoints.dart';
+import 'package:dio/dio.dart';
+
+class DioClient {
+  DioClient._();
+
+  static Dio create() {
+    final options = BaseOptions(
+      baseUrl: Urls.base,
+      connectTimeout: const Duration(seconds: 8),
+      receiveTimeout: const Duration(seconds: 12),
+      sendTimeout: const Duration(seconds: 12),
+      responseType: ResponseType.json,
+      headers: {
+        HttpHeaders.acceptHeader: 'application/json',
+        HttpHeaders.contentTypeHeader: 'application/json',
+      },
+    );
+
+    final dio = Dio(options);
+
+    dio.interceptors.add(
+      LogInterceptor(
+        request: true,
+        requestHeader: false,
+        requestBody: true,
+        responseHeader: false,
+        responseBody: true,
+        error: true,
+
+        logPrint: (object) {
+          // ignore: avoid_print
+          print(object);
+        },
+      ),
+    );
+
+    return dio;
+  }
+}
